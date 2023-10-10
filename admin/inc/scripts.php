b7fa79050359832782fc338c5e47dd382e5dc733
anandkamat-megotech <anand.k@megotechnologies.com>
2023-10-10 09:45:22 +0530

    changes

--- admin/inc/scripts.php
+++ admin/inc/scripts.php
@@ -12,4 +12,4 @@
 
 <script src="assets/plugins/moment/moment.min.js"></script>
 <script src="assets/js/bootstrap-datetimepicker.min.js"></script>
-<script src="assets/js/script.js"></script>
+<script src="assets/js/script.js?v=0.35"></script>
